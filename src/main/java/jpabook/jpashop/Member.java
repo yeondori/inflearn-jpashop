36ca63e8d3098e624ac66a7610af5af3b31ffee3
yeondori <yeonseo.jung0@gmail.com>
2023-11-03 23:35:27 +0900

    add: GeneratedValue strategy 명세

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +10,8 @@ import lombok.Setter;
 @Entity
 @Getter @Setter
 public class Member {
-    @Id @GeneratedValue
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
 
