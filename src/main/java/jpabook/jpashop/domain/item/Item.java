1309acd441bbba663c1da4c1255cbb3f3f2a4a97
yeondori <yeonseo.jung0@gmail.com>
2023-11-16 14:51:39 +0900

    feat: 재고 수량 증가 로직 추가(섹션 5)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -23,4 +23,11 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+
 }
