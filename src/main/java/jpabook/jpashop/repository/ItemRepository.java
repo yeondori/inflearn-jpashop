85dfe66e2af652843472f2802c95217322215b81
yeondori <yeonseo.jung0@gmail.com>
2023-11-16 14:58:49 +0900

    feat: ItemRepository 생성(섹션 5)

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+}
